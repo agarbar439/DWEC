1943265a5b54ab36838c716d9b6fa3633eeee699
agarbar439 <ggarciabarreraantonio@gmail.com>
2024-10-03 17:05:07 +0200

    Mostrar Texto con boton

--- Tema2/2_5_0_practica/script.js
+++ Tema2/2_5_0_practica/script.js
@@ -1,10 +1,51 @@
 // Ejercicio 3
 //Funcion para cambiar el color de fondo
 function cambiarColor() {
-    var todo = document.body; // Guarda en variable todo el body
-    todo.classList.toggle("dark-mode"); // Selecciona el elemento del css
- }
+   var todo = document.body; // Guarda en variable todo el body
+   todo.classList.toggle("dark-mode"); // Selecciona el elemento del css
+}
 
- function masTexto(){
-    var arr1 = 'Funcionamiento energía solar fotovoltaica ¿Cómo funcionan las plantas fotovoltaicas? Proyectos Iberdrola Ingeniería Energía fotovoltaicaLa energía solar fotovoltaica es aquella que se obtiene al convertir la luz solar en electricidad empleando una tecnología basada en el efecto fotoeléctrico. Se trata de un tipo de energía renovable, inagotable y no contaminante que puede producirse en instalaciones que van desde los pequeños generadores para autoconsumo hasta las grandes plantas fotovoltaicas. Descubre cómo funcionan estos enormes campos solares ';
- }
+var textos = [
+   'Funcionamiento energía solar fotovoltaica: ¿Cómo funcionan las plantas fotovoltaicas? Proyectos Iberdrola Ingeniería Energía fotovoltaica.',
+   'La energía eólica, que transforma en electricidad la fuerza de un recurso inagotable como el viento, es una apuesta sostenible y de valor para el futuro. El aprovechamiento del viento requiere de la instalación de parques eólicos, ya sea en tierra o en alta mar, con decenas de aerogeneradores.',
+   'El uso eficiente de la energía, a veces simplemente llamado eficiencia energética o ahorro energético, es el objetivo de reducir la cantidad de energía requerida para proporcionar productos y servicios.'
+];
+
+// Funcion para mostrar y esconder texto
+/*function verTexto() {
+   document.getElementById("mas1").textContent = textos[0];
+   document.getElementById("mas2").textContent = textos[1];
+   document.getElementById("mas3").textContent = textos[2];
+
+   var x = document.getElementById("text1")
+   if (x.style.display === "none") {
+      x.style.display = "block";
+   } else {
+      x.style.display = "none";
+   }
+
+   var y = document.getElementById("text2")
+   if (y.style.display === "none") {
+      y.style.display = "block";
+   } else {
+      y.style.display = "none";
+   }
+
+   var z = document.getElementById("text3")
+   if (z.style.display === "none") {
+      z.style.display = "block";
+   } else {
+      z.style.display = "none";
+   }
+}
+*/
+function verTexto(index) {
+   document.getElementById("mas" + (index + 1)).textContent = textos[index];
+
+   var elemento = document.getElementById("text" + (index + 1));
+   if (elemento.style.display === "none") {
+      elemento.style.display = "block";
+   } else {
+      elemento.style.display = "none";
+   }
+}
