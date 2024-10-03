d7e73f2486240e8094c97715390fb94f9a6e9bdb
agbarrera <antoniogarci0309@gmail.com>
2024-10-03 11:24:43 +0200

    Practica

---
+++ Tema2/2_5_0_practica/script.js
@@ -0,0 +1,10 @@
+// Ejercicio 3
+//Funcion para cambiar el color de fondo
+function cambiarColor() {
+    var todo = document.body; // Guarda en variable todo el body
+    todo.classList.toggle("dark-mode"); // Selecciona el elemento del css
+ }
+
+ function masTexto(){
+    var arr1 = 'Funcionamiento energía solar fotovoltaica ¿Cómo funcionan las plantas fotovoltaicas? Proyectos Iberdrola Ingeniería Energía fotovoltaicaLa energía solar fotovoltaica es aquella que se obtiene al convertir la luz solar en electricidad empleando una tecnología basada en el efecto fotoeléctrico. Se trata de un tipo de energía renovable, inagotable y no contaminante que puede producirse en instalaciones que van desde los pequeños generadores para autoconsumo hasta las grandes plantas fotovoltaicas. Descubre cómo funcionan estos enormes campos solares ';
+ }
