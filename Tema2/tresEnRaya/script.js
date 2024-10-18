9aa7937c954c5a5ad91adfde2898a5c1483e33ae
agarbar439 <ggarciabarreraantonio@gmail.com>
2024-10-18 17:13:42 +0200

    Tres en raya

--- Tema2/tresEnRaya/script.js
+++ Tema2/tresEnRaya/script.js
@@ -27,15 +27,13 @@ function pintarValores() {
 
   // Si el juego esta iniciado
   if (juegoIniciado) {
-    if (divPulsado && divPulsado.textContent == "") {
-      // Comprobar que la celda este vacia
+    if (divPulsado && divPulsado.textContent == "") { // Comprobar que la celda este vacia
       if (turnoJugador == "X") {
         // Si el turno es X
         divPulsado.innerHTML = "X"; // Pintar X
         tablero.splice(idPulsado, 1, "X"); // Insertar en una posicion del array el turno, (1 para reemplazar el contenido de la posicion)
-
         // Cambiar turno
-        turnoJugador = "O"; // Cambiar turno
+        turnoJugador = "O";
       } else if (turnoJugador == "O") {
         // Si el turno es de O
         divPulsado.innerHTML = "O"; // Pintar O
@@ -43,12 +41,50 @@ function pintarValores() {
 
         turnoJugador = "X"; // Cambiar turno
       }
-
+      // Mensaje del turno de jugador actual
       mensajeTurno.innerText = `Turno de Jugador: ${turnoJugador}`; // Mostrar el turno de cada jugador
     } else {
-      mensajeTurno.innerText = " Juego terminado";
+      mensajeTurno.innerText = "Juego terminado";
       juegoIniciado = false;
     }
   }
   console.log(tablero);
+  
 }
+
+// Funcion comprobar ganador
+function comprobarGanador() {
+  for (let i = 0; i < tablero.length; i++) {
+    // Comprobar primera fila
+    if (tablero[0] === tablero[1] && tablero[1] === tablero[2]) {
+      juegoIniciado = false;
+      let ganador = tablero[0];
+      mensajeTurno.innerText = "El ganador es: " + ganador;
+      // Comprobar segunda fila
+
+    } else if (tablero[3] === tablero[4] && tablero[4] === tablero[5]) {
+      juegoIniciado = false;
+      let ganador = tablero[3];
+      mensajeTurno.innerText = "El ganador es: " + ganador;
+      // Comprobar tercera fila
+
+    } else if (tablero[6] === tablero[7] && tablero[7] === tablero[8]) {
+      juegoIniciado = false;
+      let ganador = tablero[6];
+      mensajeTurno.innerText = "El ganador es: " + ganador;
+    }
+
+  }
+}
+// Funcion para resetear el juego
+function resetJuego() {
+  // Limpiar el array
+  tablero = ["", "", "", "", "", "", "", "", ""]; // Array con las posiciones del tablero para comprobar las posiciones ganadoras
+  juegoIniciado = true; // Iniciar el juego
+  // Limpiar el texto de cada celda
+  document.querySelectorAll(".celda").forEach((div) => {
+    div.innerHTML = "";
+  });
+  mensajeTurno.innerText = "Pulsa sobre el tablero para volver a jugar";
+}
+
